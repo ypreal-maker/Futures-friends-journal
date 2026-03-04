d6172192eb37509db9fcee6f923f0992dd715ca1
ypreal-maker <yptymreal@gmail.com>
2026-03-04 22:34:54 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class", // 👈 다크 모드 활성화 (핵심)
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
