2571a442daf513332f29c5237aec488c19b433b4
ypreal-maker <yptymreal@gmail.com>
2026-03-04 20:28:42 +0900

    add src/components/SmoothScroll.tsx

---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
+
+interface SmoothScrollProps {
+  children: React.ReactNode;
+}
+
+export default function SmoothScroll({ children }: SmoothScrollProps) {
+  const lenisRef = useRef<Lenis | null>(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.4,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 0.8,
+      touchMultiplier: 1.5,
+    });
+
+    lenisRef.current = lenis;
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    const animFrameId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(animFrameId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
